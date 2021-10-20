05a15e3cb420a2b80cf9d420eb237e61f4ea5bd5
Laura Segura Martinez <seguramartinezlaura2@gmail.com>
2021-10-20 16:43:31 -0300

    primer commit angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import {enableProdMode} from '@angular/core';
+import { ContadorModule } from './contador/contador/contador.module';
+
+enableProdMode();
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
